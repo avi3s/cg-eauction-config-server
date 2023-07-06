8afd68d367527c8168ef84025dcff199639ef4a1
avi3s <avirup.pal@gmail.com>
2023-07-06 19:32:02 +0530

    New Jar Added

--- src/main/java/com/cg/eauction/config/StartingServices.java
+++ src/main/java/com/cg/eauction/config/StartingServices.java
@@ -6,13 +6,17 @@ import org.springframework.core.io.ClassPathResource;
 
 public class StartingServices {
 
-    public static final String FILENAME = "zipkin-server-2.24.0.jar";
+    public static final String FILENAME = "zipkin-server-2.21.1-exec.jar";
+    public static final String PATH = "cd C:\\Study\\";
 
     public static void main(String[] args) {
 
         try {
             File file = new ClassPathResource(FILENAME).getFile();
-            String zipkins = file.getCanonicalPath();
+            String path = "cd " + file.getCanonicalPath();
+            path = path.replace(file.getName(), "");
+            System.err.println("path ==>> " + path);
+            String zipkins = path + " && java -jar " + FILENAME;
             System.err.println("zipkins ==>> " + zipkins);
             Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"dir && " + zipkins);
             Thread.sleep(10000);
